b57b9b2e4af4182e1b2f530ce088caf2b75d2cd0
shin0112 <wnslcosltimo12@gmail.com>
2024-01-24 23:37:40 +0900

    [add] 01152

---
+++ (01152) 단어의 개수.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define FASTIO                                                                 \
+    ios_base::sync_with_stdio(0);                                              \
+    cin.tie(0);
+
+using namespace std;
+
+int main() {
+    FASTIO;
+
+    string input;
+    int cnt = 0;
+    while (cin >> input) {
+        cnt++;
+    }
+
+    cout << cnt;
+
+    return 0;
+}
