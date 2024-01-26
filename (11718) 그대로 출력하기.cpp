a73c1ea4f625e7a934b8f91d3e41b8f96f0d0be0
shin0112 <wnslcosltimo12@gmail.com>
2024-01-27 03:15:05 +0900

    [add] 11718

---
+++ (11718) 그대로 출력하기.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define FASTIO                                                                 \
+    ios_base::sync_with_stdio(0);                                              \
+    cin.tie(0);
+
+using namespace std;
+
+int main() {
+    FASTIO;
+    string s;
+    while (getline(cin, s))
+        cout << s << '\n';
+    return 0;
+}
