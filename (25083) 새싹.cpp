d3095bc763013669fdcbd03a0f1d40e9cf1cd096
shin0112 <wnslcosltimo12@gmail.com>
2024-01-30 23:46:09 +0900

    [add] 25083

---
+++ (25083) 새싹.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define FASTIO                                                                 \
+    ios_base::sync_with_stdio(0);                                              \
+    cin.tie(0);
+
+using namespace std;
+
+int main() {
+    FASTIO;
+
+    cout << "         ,r\'\"7" << '\n';
+    cout << "r`-_   ,\'  ,/" << '\n';
+    cout << " \\. \". L_r\'" << '\n';
+    cout << "   `~\\/" << '\n';
+    cout << "      |" << '\n';
+    cout << "      |" << '\n';
+
+    return 0;
+}
