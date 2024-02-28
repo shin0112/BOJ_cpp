d69133d9b3a48fd1325750d680e9e1a245e9317e
shin0112 <wnslcosltimo12@gmail.com>
2024-02-29 01:06:09 +0900

    [add] 15894
    - 10^9는 int의 범위를 넘어가기 때문에 long long을 사용해야 함

---
+++ (15894) 수학은 체육과목입니다.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define FASTIO                                                                 \
+    ios_base::sync_with_stdio(0);                                              \
+    cin.tie(0);
+
+using namespace std;
+
+int main() {
+    FASTIO;
+    long long n;
+    cin >> n;
+    cout << 4 * n;
+    return 0;
+}
