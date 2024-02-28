30ea0bf34d3b82207e27989b17b6236dcaa6a250
shin0112 <wnslcosltimo12@gmail.com>
2024-02-29 00:54:18 +0900

    [add] 03009
    - 3개 비교 시 다른 것을 찾는 경우 operator ^ 사용

---
+++ (03009) 네 번째 점.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define FASTIO                                                                 \
+    ios_base::sync_with_stdio(0);                                              \
+    cin.tie(0);
+
+using namespace std;
+
+int main() {
+    FASTIO;
+    int x1, x2, x3, y1, y2, y3;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+    cout << (x1 ^ x2 ^ x3) << ' ' << (y1 ^ y2 ^ y3);
+
+    return 0;
+}
