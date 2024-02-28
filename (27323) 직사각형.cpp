7f0a94103e6cdecae90c9dfd87e6dd6b03fe1b3d
shin0112 <wnslcosltimo12@gmail.com>
2024-02-29 00:16:31 +0900

    [add] 27323

---
+++ (27323) 직사각형.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define FASTIO                                                                 \
+    ios_base::sync_with_stdio(0);                                              \
+    cin.tie(0);
+
+using namespace std;
+
+int main() {
+    FASTIO;
+    int A, B;
+    cin >> A >> B;
+    cout << A * B;
+    return 0;
+}
